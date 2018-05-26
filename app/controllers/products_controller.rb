7d10259ede0a76822310d1207c14601446564149
林佳志乃 <hayashiyoshino@hayashikeishinonoMacBook-puro.local>
2018-05-26 16:25:41 +0900

    showアクション定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < RankingController
 
   def show
     # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
-    @product = Product.new # 問題3ではこのコードは消して新しくコードを書いてください
+    @product = Product.find(params[:id]) # 問題3ではこのコードは消して新しくコードを書いてください
   end
 
   def search
