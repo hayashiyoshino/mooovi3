ef33ea0c9c2dac273e429626e51d6e1a22319621
林佳志乃 <{twinklestaryoshino@gmail.com}>
2018-05-28 18:17:37 +0900

    validates :nickname, praceholder ニックネーム

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar,
                                       content_type: ["image/jpg","image/jpeg","image/png"]
 
+  validates :nickname, presence: true
+
 end
