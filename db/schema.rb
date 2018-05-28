36987c460de661369d0bee83a6cd4db4a0ad23ef
林佳志乃 <{twinklestaryoshino@gmail.com}>
2018-05-28 17:21:12 +0900

    nicknameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180528014751) do
+ActiveRecord::Schema.define(version: 20180528081643) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20180528014751) do
     t.string   "avatar_content_type",    limit: 255
     t.integer  "avatar_file_size",       limit: 4
     t.datetime "avatar_updated_at"
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
